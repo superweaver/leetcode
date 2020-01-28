c65ea90eda5aa8ce64b9bb92edbe948968da2130
superweaver <evergreenwill@gmail.com>
2020-01-27 20:53:03 -0800

    add funtionality to convert.sh

--- convert.sh
+++ convert.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sed "s/\[/\{/g; s/\]/\}/g" temp
+# 1) use $@ to get all input parameters of shell, redirect it to temp and use sed
+# how to use it : ./convert.sh "[1,2,3]"
+#echo "$@" > temp && sed "s/\[/\{/g; s/\]/\}/g" temp
+# echo "$@" | sed "s/\[/\{/g; s/\]/\}/g"
+# 2) use xclip utility
+# copy content into clipboard, then run convert.sh
+echo $(xclip -o) | sed "s/\[/\{/g; s/\]/\}/g" | xclip -o
