b8b7e916a9479a8f3612f366b463809076d994af
superweaver <evergreenwill@gmail.com>
2020-01-28 20:55:04 -0800

    slight modification

--- init.sh
+++ init.sh
@@ -2,7 +2,9 @@
 file=$1
 if [ -f $file ]
 then
+    echo "no init, delete original file and run shell again to init"
     rm -i $file
+    exit
 fi
 
 echo "#include \"common.h\"" >> $file
@@ -10,6 +12,7 @@ echo "" >>$file
 echo "int main() {" >> $file
 echo "" >>$file
 echo -e "\tSolution s;" >> $file
+echo "" >>$file
 echo -e "\treturn 0;" >> $file
 echo "}" >> $file
 ls -lrt --color=auto
