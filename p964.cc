28ad7059d38515942cd199f5b42fc9853c3e03c2
superweaver <evergreenwill@gmail.com>
2020-02-04 00:22:32 -0800

    leetcode 964 Least Operators to Express Number

--- p964.cc
+++ p964.cc
@@ -1,91 +1,105 @@
 #include "common.h"
+//https://blog.csdn.net/lemonmillie/article/details/86628980
+//https://zhanghuimeng.github.io/post/leetcode-964-least-operators-to-express-number/
+//https://zxi.mytechroad.com/blog/uncategorized/leetcode-964-least-operators-to-express-number/
 class Solution {
 public:
+	// analysis
+	// 2 <= x <= 100
+    // 1 <= target <= 2 * 10^8
+	// target can be expressed in following form
+	//
+	// ----------------------target = sum(ai * x^i)          -------------------     formula 
+	//
+	// to get x^i, you need m operators m = (i == 0) ? 2 : (i); // +- is also included
+	// the total operators will be sum(ai * (mi)) - 1
+	// how to get ai
+	// 1) directly use x^i with number of ai , total operators = ai * (mi + 1)
+	// 2) write ai as (x-bi) * x^i, borrow a x^(i+1) from higher weight one, and use a negative bi = x-ai
+	// note if we use method 2), the bollowed one will be rearranged into upper weight x^(i+1) until the max one i^n, where a -i^(n+1) must applied
+	// so there may be two forms to get minimal operators for target
+	
+	// form 0: target = sum(ai * x^i), i = 0, 1, ..., n
+	// form 1: target = a0 + a1*x + ... + an*x^n - x^(n+1)
+	// dp[i][0] is the minimal operators to construct first (i+1) terms of formula using form0
+	// dp[i][1] is the minimal operators to construct first (i+1) terms of formula using form1
+	// 
+	// dp[i+1][0] = min (a, b)
+	//  a = dp[i][0] + a_i+1 * m_i+1
+	//  b = dp[i][1] + (a_i+1 + 1) * m_i+1
+	//
+	//  dp[i+1][1] = min(c, d)
+	//  c = dp[i][0] + abs(x-a_i+1)* m_i+1
+	//  c = dp[i][1] + abs(x - (a_i+1 + 1)) * m_i+1
+	//  refer to https://zhanghuimeng.github.io/post/leetcode-964-least-operators-to-express-number/
+	//  at last return dp[n][0] - 1; (first operator + or - is not needed)
     int leastOpsExpressTarget(int x, int target) {
-
-        //   2 <= x <= 100
-        //   1 <= target <= 2 * 10^8
-        //   bfs          
-        queue<long long> q;
-        unordered_set<long long> visited; // up to prev_group_value << 32 + current_value
-        q.push(x); // 0 + x = x
-        visited.insert(x);
-        int steps = 0;
-        while(!q.empty()) {
-            size_t size = q.size();
-            while(size--) {
-                auto current = q.front();
-                q.pop();
-                long long currentValue = current & 0xFFFFFFFF;
-                if (currentValue == (long long)target) {
-                    return steps;
-                }
-                long long previousValue = current >>= 32;
-                for (int i = ADD; i <= OPERATORS; ++i) {
-                    long long nextKey = getNextKey(previousValue, currentValue, i, x);
-                    if (nextKey) {
-                        if(!visited.count(nextKey)) {
-                           visited.insert(nextKey);
-                           q.push(nextKey);
-                        }
-                    }
-                }
-            }
-            steps++;
-        }
-        return -1;
-    }
+		// convert target to base x
+		vector<int> coef;
+		while (target) {
+			coef.push_back(target % x);
+			target /= x;
+		}
+		int n = coef.size();
+		coef.push_back(0); // tricky
+		vector<vector<int>> dp(n+1, vector<int>(2, 0)); 
+		// base case 
+		dp[0][0] = 2 * coef[0];
+		dp[0][1] = 2 * abs(x - coef[0]);
+		for (int i = 1; i <= n; ++i) {
+			dp[i][0] = min(dp[i-1][0] + coef[i] * i, dp[i-1][1] +( coef[i] + 1) * i);
+			dp[i][1] = min(dp[i-1][0] + abs(x - coef[i]) * i, dp[i-1][1] + abs(x - (1 + coef[i])) * i);
+		}
+		return dp[n][0] - 1; // tricky
+	}
+	// convert it to a graph problem
+	int leastOpsExpressTarget_pq(int x, int target) {
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+		unordered_set<int> s;
+		q.emplace(0, target); // 0 is steps, target is current node
+							  // find the shortest path to node 0
+		while (!q.empty()) {
+			const int c = q.top().first; // current steps
+			const int t = q.top().second;
+			q.pop();
+			if (t == 0) return c - 1;
+			if (s.count(t)) continue; //have visited previously by another path
+			s.insert(t); // not visited ? now visit it
+			int n = log(t) / log(x);
+			int l = t - pow(x, n);        // l <= t
+			q.emplace(c + (n == 0 ? 2 : n), l);
+			// try anoterh way: get a bigger number and x^(n+1) and then get back
+			int r = pow(x, n + 1) - t;        
+			q.emplace(c + n + 1, r);
+		}
+		return -1;
+	}
+public:
+  int leastOpsExpressTarget_dp(int x, int target) {
+    return dp(x, target);
+  }
 private:
-    enum {ADD, MINUS, MULTIPLY, DIVIDE, OPERATORS};
-    const int lowerBound = 1; 
-    const int upperBound = 200000000;
-
-    long long getNextKey(long long pv, long long cv, int operation, int x) {
-        // lastop is for group
-        int lastOp = cv > pv ? ADD : MINUS;
-        long long cgv = (lastOp == ADD) ? (cv - pv) : (pv - cv);
-        if (operation == ADD || operation == MINUS) {
-            pv = cv;
-            cv = (operation == ADD) ? (cv + x) : (cv - x);
-            if (cv < 0 || cv > INT_MAX) {
-                return 0;
-            }
-            return ((pv << 32) + cv);
-        }
-        // MULTIPLY and DIVIDE;
-        if (cgv == 1) {
-            return 0;
-        } else {
-            if (operation == DIVIDE) {
-                if (cgv == x) { 
-                    cgv = 1; 
-                    cv = (lastOp == ADD) ? (pv + cgv) : (pv - cgv);
-                    if (cv < 0 || cv > INT_MAX) {
-                        return 0;
-                    }
-                    return ((pv << 32) + cv);
-                }
-                else {
-                    return 0;
-                }
-            } else { // Multipy
-                cv = (lastOp == ADD) ? (pv + cgv * x) : (pv - cgv * x);
-                if (cv < 0 || cv > INT_MAX) {
-                    return 0;
-                }
-                return ((pv << 32) + cv);
-
-            }
-        }
-    }
+  unordered_map<int, int> m_;
+  int dp(int x, int t) {
+	  if (t == 0) return 0;
+	  if (t < x) return min(2 * t - 1, 2 * (x - t));
+	  if (m_.count(t)) return m_.at(t);
+	  int k = log(t) / log(x);
+	  long p = pow(x, k);
+	  if (t == p) return m_[t] = k - 1;
+	  int ans = dp(x, t - p) + k; // t - p < t
+	  long left = p * x - t;
+	  if (0 <= left < t) // only rely on smaller sub-problems
+		  ans = min(ans, dp(x, left) + k + 1);
+	  return m_[t] = ans;
+  }
 
 };
 int main() {
-    int x = 3, target = 19; // 5
-    x = 5, target = 501; // 8
-    x = 100, target = 100000000;
-    x = 2, target = 125046;
-    Solution s;
-    cout << s.leastOpsExpressTarget(x, target) << endl;
-    return 0;
+	int x = 5;
+	int target = 501; // 8
+	// x = 3, target = 19; // 5
+	Solution s;
+	cout << s.leastOpsExpressTarget(x, target) << endl;
+	return 0;
 }
