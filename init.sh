ce04ed813cb3cb654c1dde9f71e3206b576ff094
superweaver <evergreenwill@gmail.com>
2020-02-11 23:47:58 -0800

    change init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 file=$1
-if [ -f $file ]
-then
+if [[ -f $file ]]; then
     echo "no init, delete original file and run shell again to init"
     rm -i $file
     exit
+else
+	echo "#include \"common.h\"" >> $file
+	echo "" >>$file
+	echo "int main() {" >> $file
+	echo "" >>$file
+	echo -e "\tSolution s;" >> $file
+	echo "" >>$file
+	echo -e "\treturn 0;" >> $file
+	echo "}" >> $file
+	ls -lrt --color=auto $file
+	vim $file
 fi
 
-echo "#include \"common.h\"" >> $file
-echo "" >>$file
-echo "int main() {" >> $file
-echo "" >>$file
-echo -e "\tSolution s;" >> $file
-echo "" >>$file
-echo -e "\treturn 0;" >> $file
-echo "}" >> $file
-ls -lrt --color=auto $file
-#vi $file
+
